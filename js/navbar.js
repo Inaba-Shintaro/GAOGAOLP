e49fe25e91a8d59821e8efa86114b7cbe0fa67de
inaba <inaba.shintaro.6221@gmail.com>
2019-12-06 10:20:58 +0900

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,14 @@
+$(function() {
+  // 処理
+  var navPos = jQuery("#mainNav").offset().top; // グローバルメニューの位置
+
+  jQuery(window).on("scroll", function() {
+    if (jQuery(this).scrollTop() > navPos) {
+      jQuery("body").css("padding-top", 45.656); // グローバルメニューの高さ（縮んだ時の高さ）
+      jQuery("#mainNav").addClass("m_fixed");
+    } else {
+      jQuery("body").css("padding-top", 0);
+      jQuery("#mainNav").removeClass("m_fixed");
+    }
+  });
+});
